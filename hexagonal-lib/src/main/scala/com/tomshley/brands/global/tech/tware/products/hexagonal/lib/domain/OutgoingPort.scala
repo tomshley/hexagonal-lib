9dd251147901ed65e647938f8353c33d23feea68
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-10-28 08:30:27 -0400

    WIP tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-lib-jvm#2
    https://gitlab.com/tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-lib-jvm/-/issues/2

--- hexagonal-lib/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/lib/domain/OutgoingPort.scala
+++ hexagonal-lib/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/lib/domain/OutgoingPort.scala
@@ -1,5 +1,3 @@
 package com.tomshley.brands.global.tech.tware.products.hexagonal.lib.domain
 
-trait OutgoingPort[T1 <: Model, T2 <: OutgoingModel] extends Port[T1, T2] {
-  override def liftPort: T2 = ???
-}
+trait OutgoingPort[T1 <: Model, T2 <: OutgoingModel] extends Port[T1, T2]
--- hexagonal-lib/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/lib/domain/Port.scala
+++ hexagonal-lib/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/lib/domain/Port.scala
@@ -1,6 +1,5 @@
 package com.tomshley.brands.global.tech.tware.products.hexagonal.lib.domain
 
 trait Port[T1 <: Model, T2 <: Model] {
-  val inboundModel:T1
-  def liftPort: T2 = ???
+  def execute(inboundModel:T1): T2 = ???
 }
