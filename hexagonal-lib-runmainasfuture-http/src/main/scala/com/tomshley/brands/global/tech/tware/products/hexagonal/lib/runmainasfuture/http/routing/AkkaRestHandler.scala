d90a76586eaf10a0f8979665331ed63222fec1cc
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-10-30 14:53:04 -0400

    WIP tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-lib-jvm#2
    https://gitlab.com/tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-lib-jvm/-/issues/2

---
+++ hexagonal-lib-runmainasfuture-http/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/lib/runmainasfuture/http/routing/AkkaRestHandler.scala
@@ -0,0 +1,9 @@
+package com.tomshley.brands.global.tech.tware.products.hexagonal.lib.runmainasfuture.http.routing
+
+import akka.http.scaladsl.server.Route
+
+trait AkkaRestHandler {
+  lazy val routes: Seq[Route] = throw new NotImplementedError(
+    "This method must be implemented to use services. Ok if not using services"
+  )
+}
